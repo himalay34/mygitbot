35e2b98ae15ffdd8a9645b0e3b8c8b97d11aa1b3
himalay34 <ema.serve@gmail.com>
2019-09-06 02:07:58 +0600

    Update player.js

--- src/commands/coc/player.js
+++ src/commands/coc/player.js
@@ -31,20 +31,20 @@ exports.run = async(bot, msg, args, fn) => {
       await embed
         //.setTitle(`${data.name} ${data.tag}`)
         .setAuthor(data.name + ' '+ data.tag, data.league.iconUrls.tiny, url)
-        .addField(`***Town Hall Level***`, `${data.townHallLevel}`, true)
-        .addField(`***Builder Hall Level***`, `${data.builderHallLevel}`, true)
-        .addField('***Exp Level***', data.expLevel, true)
-        .addField(`***War Stars***`, `${data.warStars}`, true)
+        .addField(`❯ ***Town Hall Level***`, `${data.townHallLevel}`, true)
+        .addField(`❯ ***Builder Hall Level***`, `${data.builderHallLevel}`, true)
+        .addField('❯ ***Exp Level***', data.expLevel, true)
+        .addField(`❯ ***War Stars***`, `${data.warStars}`, true)
         
-        .addField(`***League***`, `${data.league.name}`, true)
-        .addField('***Trophies***', ':trophy: '+data.trophies, true)
-        .addField('***Best Trophies***', ':trophy: '+data.bestTrophies, true)
-        .addField(`***Attack Wins***`, `${data.attackWins}`, true)
-        .addField(`***Defense Wins***`, `${data.defenseWins}`, true)
-        .addField('***Donations***',  `:arrow_up: ${data.donations}`, true)
-        .addField('***Donations Received***', `:arrow_down: ${data.donationsReceived}`, true)
-        .addField('***D:R Ratio***', '≈ ' + Math.round(data.donations/data.donationsReceived), true)
-        .addField('***Versus Info:***', `\`\`\`Versus Trophies: ${data.versusTrophies} \nBest Versus Trophies: ${data.bestVersusTrophies} \nVersus Battle Wins: ${data.versusBattleWins} \`\`\``)
+        .addField(`❯ ***League***`, `${data.league.name}`, true)
+        .addField('❯ ***Trophies***', ':trophy: '+data.trophies, true)
+        .addField('❯ ***Best Trophies***', ':trophy: '+data.bestTrophies, true)
+        .addField(`❯ ***Attack Wins***`, `${data.attackWins}`, true)
+        .addField(`❯ ***Defense Wins***`, `${data.defenseWins}`, true)
+        .addField('❯ ***Donations***',  `:arrow_up: ${data.donations}`, true)
+        .addField('❯ ***Donations Received***', `:arrow_down: ${data.donationsReceived}`, true)
+        .addField('❯ ***D:R Ratio***', '≈ ' + Math.round(data.donations/data.donationsReceived), true)
+        .addField('❯ ***Versus Info:***', `\`\`\`Versus Trophies: ${data.versusTrophies} \nBest Versus Trophies: ${data.bestVersusTrophies} \nVersus Battle Wins: ${data.versusBattleWins} \`\`\``)
         
         .setURL('http://bgctmcwarriors.ml/api/player?tag='+data.tag);
         if(data.heroes){
@@ -59,16 +59,16 @@ exports.run = async(bot, msg, args, fn) => {
                 str +=  `${fn.emoji(bot,'grandwarden')} ${data.heroes[2].name} (${data.heroes[2].level})  `
             }
 
-            embed.addField('***Heroes***', str);
+            embed.addField('❯ ***Heroes***', str);
         }
         if(data.clan){
-        embed.addField(`***Clan***`, `\`\`\`${data.clan.name} \nTag: ${data.clan.tag} \nClan Level: ${data.clan.clanLevel}\`\`\``)
-        .addField(`***Role***`, `:shield: ${data.role}`, true);
+        embed.addField(`❯ ***Clan***`, `\`\`\`${data.clan.name} \nTag: ${data.clan.tag} \nClan Level: ${data.clan.clanLevel}\`\`\``)
+        .addField(`❯ ***Role***`, `:shield: ${data.role}`, true);
         }
         
     if(data.legendStatistics){
           embed
-          .addField(`***Legend League Statistics***`,`\`\`\`Best Season: ${data.legendStatistics.bestSeason.id} \nTrophies: ${data.legendStatistics.bestSeason.trophies} \nRank: ${data.legendStatistics.bestSeason.rank} \`\`\``);
+          .addField(`❯ ***Legend League Statistics***`,`\`\`\`Best Season: ${data.legendStatistics.bestSeason.id} \nTrophies: ${data.legendStatistics.bestSeason.trophies} \nRank: ${data.legendStatistics.bestSeason.rank} \`\`\``);
         }
     
     embed.setTimestamp()
