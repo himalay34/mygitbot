dec92f278cb7d3df47cfb2636fdd2f5eedc6e5b8
himalay34 <ema.serve@gmail.com>
2019-09-05 21:03:00 +0600

    Create player.js

---
+++ src/commands/coc/player.js
@@ -0,0 +1,38 @@
+const url = 'http://bgctmcwarriors.ml/api/raw?uri=players/' //2Y0L98GRV
+
+exports.run = async(bot, msg, args, fn) => {
+    msg.delete().catch(O_o => {});
+    
+    let tag = ''
+    
+    if (!args[0].length){
+        tag = '#8VQUCLJQ'//implement attached id here
+    } else {
+        tag = args[0]// grab clan tag
+    }
+    
+    // check is it starts with #, then remove #
+    if(tag[0] == '#')
+        tag = tag.substring(1);
+        
+    const embed = fn.embed()
+    console.log(tag)
+    try {
+        await axios.get(url+tag).
+            .then(res=>{
+
+                console.info(typeof(res.data))
+                console.info(res.data)
+            })
+
+    } catch (error) {
+        throw error
+    }
+};
+
+exports.info = {
+    name: 'player',
+    usage: 'player [tag]',
+    description: 'Shows player informations',
+    group: 'coc'
+};
